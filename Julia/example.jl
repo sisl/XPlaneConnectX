d89483705fe516c1c457535cd2a04ab73c3a0ada
MarcSchlichting <schlichting.marc@gmail.com>
2024-08-06 14:52:56 -0700

    comments in example.jl and example.py

--- Julia/example.jl
+++ Julia/example.jl
@@ -12,25 +12,22 @@ function print_state(state)
 end
 
 observed_drefs = [
-    ("sim/flightmodel/position/phi", 10),
-    ("sim/flightmodel/position/theta", 10),
-    ("sim/flightmodel/position/psi", 10),
-    ("sim/flightmodel/position/P", 10),
-    ("sim/flightmodel/position/Q", 10),
-    ("sim/flightmodel/position/R", 10),
-    ("sim/flightmodel/position/y_agl", 10)
+    ("sim/flightmodel/position/phi", 10),   # roll angle
+    ("sim/flightmodel/position/theta", 10), # pitch angle
+    ("sim/flightmodel/position/psi", 10),   # yaw angle
 ]
 
 xpc = XPlaneConnect2(observed_drefs)
 
-sleep(1)
-ldg_light = true
+sleep(0.5)          # this is just a safety buffer    
+
+ldg_light = true    # we want to toggle the landing light at 1s intervals
 
 while true
     global ldg_light
-    print_state(xpc.current_data)
-    set_dref(xpc, "sim/cockpit/electrical/landing_lights_on", ldg_light)
-    ldg_light = !ldg_light
-    send_cmnd(xpc, "sim/operation/screenshot")
+    print_state(xpc.current_data)   # print the current observed drefs
+    set_dref(xpc, "sim/cockpit/electrical/landing_lights_on", ldg_light)    # toggle the landing lights
+    ldg_light = !ldg_light  
+    send_cmnd(xpc, "sim/operation/screenshot")  # take a screenshot (screenshots are stored in /.../X-Plane 12/Output/screenshots)
     sleep(1)
 end
--- Python3/example.py
+++ Python3/example.py
@@ -6,22 +6,21 @@ def print_state(state):
     print('-'*98)
     for k in state.keys():
         print(f"{k:<40} {state[k]['value']:<30} {state[k]['timestamp']}")
-        
-xpc = XPlaneConnect2(observed_drefs=[["sim/flightmodel/position/phi",10],
-                                       ["sim/flightmodel/position/theta",10],
-                                       ["sim/flightmodel/position/psi",10],
-                                       ["sim/flightmodel/position/P",10],
-                                       ["sim/flightmodel/position/Q",10],
-                                       ["sim/flightmodel/position/R",10],
-                                       ["sim/flightmodel/position/y_agl",10],
-                                        ])
 
-time.sleep(1)
-ldg_light = True
+observed_drefs=[["sim/flightmodel/position/phi",10],     # roll angle
+                ["sim/flightmodel/position/theta",10],   # pitch angle
+                ["sim/flightmodel/position/psi",10],     # yaw angle
+                ]   
+   
+xpc = XPlaneConnect2(observed_drefs)
+
+time.sleep(0.5)     # this is just a safety buffer    
+
+ldg_light = True    # we want to toggle the landing light at 1s intervals
 
 while True:
-    print_state(xpc.current_data)
-    xpc.set_dref('sim/cockpit/electrical/landing_lights_on',ldg_light)
+    print_state(xpc.current_data)       # print the current observed drefs
+    xpc.set_dref('sim/cockpit/electrical/landing_lights_on',ldg_light)  # toggle the landing lights
     ldg_light = not ldg_light
-    xpc.send_cmnd('sim/operation/screenshot')
+    xpc.send_cmnd('sim/operation/screenshot')   # take a screenshot (screenshots are stored in /.../X-Plane 12/Output/screenshots)
     time.sleep(1)
